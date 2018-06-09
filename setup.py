f4d133288a9ea4e23aab71abc652434e1b2534c6
z00nx 0 <z00nx0@gmail.com>
2018-06-09 22:57:20 +1000

    Added dumpit2volatilityrc

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='git-credential-keepasshttp',
+    version='1.0',
+    description='Collection of scripts',
+    url='https://github.com/z00nx/z00nxSuite',
+    author='z00nx 0',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='volatility dumpit python',
+    scripts=['bin/dumpit2volatilityrc'],
+)
